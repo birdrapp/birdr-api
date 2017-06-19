734e9d7f4c442fbe738a94ea91591c2a877bcda7
Matthew Williams <matthew.williams@me.com>
2017-06-19 17:30:06 +0100

    Add shoulda-matchers
    Seriously DRYs up the model testing with custom Rails
    matchers.
    
    https://github.com/thoughtbot/shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,79 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let (:user) { User.new first_name: 'Matt', last_name: 'Williams', email: 'matt@williams.com', password: 'secret' }
+  describe 'validations' do
+    subject { User.new first_name: 'Matt', last_name: 'Williams', email: 'matt@williams.com', password: 'secret' }
 
-  describe "#first_name" do
-    it "is required" do
-      user.first_name = ''
-      expect(user.valid?).to be false
-    end
+    it { should validate_presence_of(:first_name) }
+    it { should validate_length_of(:first_name).is_at_most(255) }
 
-    it 'cannot be longer than 255 characters' do
-      user.first_name = 'm' * 256
-      expect(user.valid?).to be false
-    end
-  end
+    it { should validate_presence_of(:last_name) }
+    it { should validate_length_of(:last_name).is_at_most(255) }
 
-  describe '#last_name' do
-    it 'is required' do
-      user.last_name = ''
-      expect(user.valid?).to be false
-    end
-
-    it 'cannot be longer than 255 characters' do
-      user.last_name = 'w' * 300
-      expect(user.valid?).to be false
-    end
-  end
-
-  describe '#email' do
-    it 'is required' do
-      user.email = ''
-      expect(user.valid?).to be false
-    end
-
-    it 'cannot be longer than 255 characters' do
-      user.email = 'w' * 300
-      expect(user.valid?).to be false
-    end
-
-    it 'must match an email address pattern' do
-      user.email = 'invalid.com'
-      expect(user.valid?).to be false
-    end
-
-    it 'must be unique' do
-      duplicate_user = user.dup
-      duplicate_user.email = user.email.upcase
-      user.save
-      expect(duplicate_user.valid?).to be false
-    end
-
-    it 'should save a downcased email address' do
+    it { should validate_presence_of(:email) }
+    it { should validate_length_of(:email).is_at_most(255) }
+    it { should allow_value('test@example.com').for(:email) }
+    it { should_not allow_value('test').for(:email) }
+    it { should_not allow_value('test@com').for(:email) }
+    it { should_not allow_value('example.com').for(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it 'should downcase the email address' do
       mixed_case_email = 'ShOuT@mE.CoM'
-      user.email = mixed_case_email
-      user.save
-      assert_equal mixed_case_email.downcase, user.reload.email
-    end
-  end
-
-  describe '#password' do
-    it 'should be present (nonblank)' do
-      user.password = ' ' * 6
-      expect(user.valid?).to be false
+      subject.update_attributes email: mixed_case_email
+      assert_equal mixed_case_email.downcase, subject.reload.email
     end
 
-    it 'should have minimum length of 6' do
-      user.password = '123'
-      expect(user.valid?).to be false
-    end
-
-    it 'creates a hash on save' do
-      user.save
-      expect(user.password_digest).to_not be nil
-      expect(user.password).to_not be nil
-      expect(user.password).to_not eq(user.password_digest)
-    end
+    it { should have_secure_password }
+    it { should validate_length_of(:password).is_at_least(6) }
   end
 end
