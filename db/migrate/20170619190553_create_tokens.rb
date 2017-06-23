f3b040ad4f3854462b6126be4ad8e75049452fc7
Matthew Williams <matthew.williams@me.com>
2017-06-23 07:22:43 +0100

    Make user_id mandatory in tokens

--- db/migrate/20170619190553_create_tokens.rb
+++ db/migrate/20170619190553_create_tokens.rb
@@ -1,7 +1,7 @@
 class CreateTokens < ActiveRecord::Migration[5.1]
   def change
     create_table :tokens, id: :uuid do |t|
-      t.references :user, type: :uuid, foreign_key: true
+      t.references :user, type: :uuid, foreign_key: true, null: false
 
       t.timestamps
     end
